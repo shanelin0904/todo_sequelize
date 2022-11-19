dcd920566a9fb697c7ce23b100f511736bb264b7
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-11-20 03:01:09 +0800

    modify routes setting

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
 const session = require('express-session')
-const passport = require('passport')
+
 const usePassport = require('./config/passport')
+const flash = require('connect-flash')
+const routes = require('./routes')
+
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
+
 const app = express()
 const PORT = 3000
 
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
 
 
+// vie engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
@@ -23,68 +28,25 @@ app.use(session({
 }))
 
 usePassport(app)
+// middlewares
+app.use(express.urlencoded({ extended: true }))// 解析request body
+app.use(methodOverride('_method'))//改寫HTTP method
 
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
-
-
-app.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos: todos }) })
-    .catch((error) => { return res.status(422).json(error) })
+app.use(flash())
+// 取得flash message
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.register_errs = req.flash('register_errs')
+  res.locals.deleted_msg = req.flash('deleted_msg')
+  next()
 })
 
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-app.post('/users/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
-
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-app.post('/users/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.findOne({ where: { email } }).then(user => {
-    if (user) {
-      console.log('User already exists')
-      return res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    }
-    return bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(password, salt))
-      .then(hash => User.create({
-        name,
-        email,
-        password: hash
-      }))
-      .then(() => res.redirect('/'))
-      .catch(err => console.log(err))
-  })
-})
-
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
-})
+// router
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
